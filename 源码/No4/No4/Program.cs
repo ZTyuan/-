a67502ae738c78cd172f3e59a8a22430e929def0
tyz <879486190@qq.com>
2024-12-05 19:57:40 +0800

    第四题

---
+++ 源码/No4/No4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class TalentAssessmentSystem
+{
+    public static double FindMedianTalentIndex(int[] fireAbility, int[] iceAbility)
+    {
+        int totalLength = fireAbility.Length + iceAbility.Length;
+        int[] merged = new int[totalLength];
+        int i = 0, j = 0, k = 0;
+
+        while (i < fireAbility.Length && j < iceAbility.Length)
+        {
+            if (fireAbility[i] < iceAbility[j])
+            {
+                merged[k++] = fireAbility[i++];
+            }
+            else
+            {
+                merged[k++] = iceAbility[j++];
+            }
+        }
+
+        while (i < fireAbility.Length)
+        {
+            merged[k++] = fireAbility[i++];
+        }
+
+        while (j < iceAbility.Length)
+        {
+            merged[k++] = iceAbility[j++];
+        }
+
+        if (totalLength % 2 == 0)
+        {
+            return (merged[totalLength / 2 - 1] + merged[totalLength / 2]) / 2.0;
+        }
+        else
+        {
+            return merged[totalLength / 2];
+        }
+    }
+    static void Main()
+    { }
+}
+
---
+++ 源码/No4/No4Tests/TalentAssessmentSystemTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class TalentAssessmentSystemTests
+    {
+        [TestMethod()]
+        public void TestFindMedianTalentIndex()
+        {
+            Assert.AreEqual(8, TalentAssessmentSystem.FindMedianTalentIndex(new int[] { 1, 3, 7, 9, 11 }, new int[] { 2, 4, 8, 10, 12, 14 }));
+        }
+         [TestMethod()]
+    public void TestFindMedianTalentIndex_BothArraysNotEmpty()
+    {
+        int[] fireAbility = {1, 3, 7, 9, 11};
+        int[] iceAbility = {2, 4, 8, 10, 12, 14};
+        double median = TalentAssessmentSystem.FindMedianTalentIndex(fireAbility, iceAbility);
+        Assert.AreEqual(8, median);
+    }
+
+    [TestMethod()]
+    public void TestFindMedianTalentIndex_OneArrayEmpty()
+    {
+        int[] fireAbility = {};
+        int[] iceAbility = {2, 4, 8, 10, 12, 14};
+        double median = TalentAssessmentSystem.FindMedianTalentIndex(fireAbility, iceAbility);
+        Assert.AreEqual(9, median);
+    }
+
+    [TestMethod()]
+    public void TestFindMedianTalentIndex_OddTotalLength()
+    {
+        int[] fireAbility = {1, 3, 7};
+        int[] iceAbility = {2, 4, 8, 10, 12};
+        double median = TalentAssessmentSystem.FindMedianTalentIndex(fireAbility, iceAbility);
+        Assert.AreEqual(5.5, median);
+    }
+
+    [TestMethod()]
+    public void TestFindMedianTalentIndex_EvenTotalLength()
+    {
+        int[] fireAbility = {1, 3, 7, 9};
+        int[] iceAbility = {2, 4, 8, 10, 12};
+        double median = TalentAssessmentSystem.FindMedianTalentIndex(fireAbility, iceAbility);
+        Assert.AreEqual(7, median);
+    }
+
+    [TestMethod()]
+    public void TestFindMedianTalentIndex_ElementsOverlap()
+    {
+        int[] fireAbility = {1, 3, 7, 9, 11};
+        int[] iceAbility = {7, 9, 11, 12, 14};
+        double median = TalentAssessmentSystem.FindMedianTalentIndex(fireAbility, iceAbility);
+        Assert.AreEqual(9, median);
+    }
+
+    [TestMethod()]
+    public void TestFindMedianTalentIndex_SameElements()
+    {
+        int[] fireAbility = {1, 2, 3};
+        int[] iceAbility = {1, 2, 3};
+        double median = TalentAssessmentSystem.FindMedianTalentIndex(fireAbility, iceAbility);
+        Assert.AreEqual(2, median);
+    }
+    }
+}
