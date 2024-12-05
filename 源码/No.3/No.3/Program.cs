6c09e7d80d10b05c9e82cd0ce4d7fb7889e2eade
ZTyuan <161200631+ZTyuan@users.noreply.github.com>
2024-12-05 18:57:15 +0800

    Update Program.cs

--- 源码/No.3/No.3/Program.cs
+++ 源码/No.3/No.3/Program.cs
@@ -2,25 +2,27 @@
 public class TreasureHuntSystem
 {
     public static int MaxTreasureValue(int[] treasures)
+{
+    if (treasures == null || treasures.Length == 0)
+        return 0;
+    if (treasures.Length == 1)
+        return treasures[0];
+    if (treasures.Length == 2)
+        return Math.Max(treasures[0], treasures[1]);
+
+    //int[] dp = new int[treasures.Length];
+    int pre= treasures[0];
+    int aft = Math.Max(treasures[0], treasures[1]);
+
+    for (int i = 2; i < treasures.Length; i++)
     {
-        if (treasures == null || treasures.Length == 0)
-            return 0;
-        if (treasures.Length == 1)
-            return treasures[0];
-        if (treasures.Length == 2)
-            return Math.Max(treasures[0], treasures[1]);
-
-        int[] dp = new int[treasures.Length];
-        dp[0] = treasures[0];
-        dp[1] = Math.Max(treasures[0], treasures[1]);
-
-        for (int i = 2; i < treasures.Length; i++)
-        {
-            dp[i] = Math.Max(dp[i - 1], dp[i - 2] + treasures[i]);
-        }
-
-        return dp[treasures.Length - 1];
+        int t = Math.Max(aft, pre + treasures[i]);
+        pre = aft;
+        aft = t;
     }
+
+    return aft;
+}
     static void Main()
     {
         Console.WriteLine(MaxTreasureValue(new int[] { 4, 3, 2, 3 }));
