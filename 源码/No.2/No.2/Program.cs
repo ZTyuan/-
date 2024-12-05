ab551759b4d15b0cc3127bc4612946d7aa6a377b
tyz <879486190@qq.com>
2024-12-05 18:04:04 +0800

    第二题

---
+++ 源码/No.2/No.2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class EnergyFieldSystem
+{
+    public static float MaxEnergyField(int[] heights)
+    {
+        if (heights == null || heights.Length < 2)
+        {
+            return 0.0f;
+        }
+
+        int n = heights.Length;
+        float maxEnergy = 0.0f;
+        int left = 0;
+        int right = n - 1;     
+        float originS = (heights[left] + heights[right]) * (right - left);
+        maxEnergy=Math.Max(maxEnergy, originS);
+        //固定左边
+        for (int i = right; i > left; i--)
+        {
+            float currS = (heights[left] + heights[i]) * (i - left);
+            if (currS >= originS)
+            { 
+                right = i;
+                originS = currS;
+            }
+        }
+        //找左边
+        for (int i = left; i < right; i++)
+        {
+            float currS = (heights[right] + heights[i]) * (right - i);
+            if (currS >= originS)
+            {
+                left = i;
+                originS = currS;
+            }
+        }
+        maxEnergy = Math.Max(maxEnergy, originS);
+        //固定右边
+        left = 0;
+        right = n - 1;
+        originS = (heights[left] + heights[right]) * (right - left);
+        for (int i = left; i <right; i++)
+        {
+            float currS = (heights[right] + heights[i]) * (right - i);
+            if (currS >= originS)
+            {
+                left = i;
+                originS = currS;
+            }
+        }
+        //找右边
+        for (int i = right; i >left; i--)
+        {
+            float currS = (heights[left] + heights[i]) * (i - left);
+            if (currS >= originS)
+            {
+                right = i;
+                originS = currS;
+            }
+        }
+        maxEnergy = Math.Max(maxEnergy, originS);
+
+        return maxEnergy/2.0f;
+    }
+    static void Main()
+    {
+        Console.WriteLine(MaxEnergyField(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+    }
+}
+
---
+++ 源码/No.2/No.2Tests/EnergyFieldSystemTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    
+    public class EnergyFieldSystemTests
+    {
+        [TestMethod]
+        public void TestMaxEnergyField()
+        {
+            Assert.AreEqual(52.5, EnergyFieldSystem.MaxEnergyField(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+        }
+        [TestMethod]
+        public void TestMaxEnergyField_WithNullInput_ReturnsZero()
+        {
+            Assert.AreEqual(0, EnergyFieldSystem.MaxEnergyField(null));
+        }
+
+        [TestMethod]
+        public void TestMaxEnergyField_WithEmptyArray_ReturnsZero()
+        {
+            Assert.AreEqual(0, EnergyFieldSystem.MaxEnergyField(new int[] { }));
+        }
+
+        [TestMethod]
+        public void TestMaxEnergyField_WithSingleElement_ReturnsZero()
+        {
+            Assert.AreEqual(0, EnergyFieldSystem.MaxEnergyField(new int[] { 10 }));
+        }
+
+        [TestMethod]
+        public void TestMaxEnergyField_WithTwoElements_ReturnsCorrectResult()
+        {
+            Assert.AreEqual(5, EnergyFieldSystem.MaxEnergyField(new int[] { 3, 7 }));
+        }
+
+        [TestMethod]
+        public void TestMaxEnergyField_WithAllElementsSameHeight_ReturnsCorrectResult()
+        {
+            Assert.AreEqual(20, EnergyFieldSystem.MaxEnergyField(new int[] { 5, 5, 5, 5, 5 }));
+        }
+
+        [TestMethod]
+        public void TestMaxEnergyField_WithLargeArray_ReturnsCorrectResult()
+        {
+            Assert.AreEqual(10004*3/2.0f, EnergyFieldSystem.MaxEnergyField(new int[] { 3 ,4 ,10000,1, 100, 4 }));
+        }
+    }
+}
